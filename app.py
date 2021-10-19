3192b468d7c192ba5e0ebe0358a7d5da3364580d
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-10-20 00:23:59 +0100

    add clauses to check if stock has momentum

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import csv
 from flask import Flask, render_template, request
 import os
 import pandas as pd
@@ -13,21 +13,29 @@ app = Flask(__name__)
 def index():
     momentum= request.args.get('momentum', None)
     stocks = {}
+
+    with open('data/s&p500.csv') as file:
+        for row in csv.reader(file):
+            stocks[row[0]] = {'company': row[1]}
     if momentum:
         data_files = os.listdir('data\daily')
         for data in data_files:
             df = pd.read_csv(f'data/daily/{data}')
             momentum_function = getattr(talib, momentum)
-
+            symbol = file.split('.')[0]
             try:
                 result = momentum_function(df['Open'], df['High'], df['Low'], df['Close'])
                 last = result.tail(1).values[0]
-                print(last)
-                if last !=0:
-                    print(f'{data} triggered {momentum}')
+                #print(last)
+                if last > 0:
+                    stocks[symbol][momentum] = 'Has momentum'
+                elif last < 0:
+                    stocks[symbol][momentum] = 'Does not have momentum'
+                else:
+                    stocks[symbol][momentum] = None
             except:
                 pass
-    return render_template('index.html', momentums=momentums)
+    return render_template('index.html', momentums=momentums, stocks=stocks)
 
 
 @app.route('/capture')
