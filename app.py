7b724ed62ccf2ca81da978d184b3f49a1455d1fa
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-10-21 01:11:03 +0100

    working project

--- app.py
+++ app.py
@@ -1,52 +1,52 @@
-import csv
-from flask import Flask, render_template, request
-import os
-import pandas as pd
-import yfinance as yfin
-from momentums import momentums
+import os, csv
 import talib
+import yfinance as yf
+import pandas
+from flask import Flask, escape, request, render_template
+from patterns import candlestick_patterns
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    momentum= request.args.get('momentum', None)
-    stocks = {}
-
-    with open('data/s&p500.csv') as file:
-        for row in csv.reader(file):
-            stocks[row[0]] = {'company': row[1]}
-    if momentum:
-        data_files = os.listdir('data\daily')
-        for data in data_files:
-            df = pd.read_csv(f'data/daily/{data}')
-            momentum_function = getattr(talib, momentum)
-            symbol = file.split('.')[0]
-            try:
-                result = momentum_function(df['Open'], df['High'], df['Low'], df['Close'])
-                last = result.tail(1).values[0]
-                #print(last)
-                if last > 0:
-                    stocks[symbol][momentum] = 'Has momentum'
-                elif last < 0:
-                    stocks[symbol][momentum] = 'Does not have momentum'
-                else:
-                    stocks[symbol][momentum] = None
-            except:
-                pass
-    return render_template('index.html', momentums=momentums, stocks=stocks)
-
-
-@app.route('/capture')
-def capture():
+@app.route('/snapshot')
+def snapshot():
     with open('data/s&p500.csv') as f:
-        symbols = f.read().splitlines()
-        for symbol in symbols:
-            ticker = symbol.split(',')[0]
-            df = yfin.download(ticker, start="2021-01-01", end="2021-10-19")
-            df.to_csv('data/daily/{}.csv'.format(ticker))
+        for line in f:
+            if "," not in line:
+                continue
+            symbol = line.split(",")[0]
+            data = yf.download(symbol, start="2020-01-01", end="2020-08-01")
+            data.to_csv('datasets/daily/{}.csv'.format(symbol))
 
     return {
-        'code': 'success'
+        "code": "success"
     }
+
+@app.route('/')
+def index():
+    pattern  = request.args.get('pattern', False)
+    stocks = {}
+
+    with open('data/s&p500.csv') as f:
+        for row in csv.reader(f):
+            stocks[row[0]] = {'company': row[1]}
+
+    if pattern:
+        for filename in os.listdir('data/daily'):
+            df = pandas.read_csv('data/daily/{}'.format(filename))
+            pattern_function = getattr(talib, pattern)
+            symbol = filename.split('.')[0]
+
+            try:
+                results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
+                last = results.tail(1).values[0]
+
+                if last > 0:
+                    stocks[symbol][pattern] = 'bullish'
+                elif last < 0:
+                    stocks[symbol][pattern] = 'bearish'
+                else:
+                    stocks[symbol][pattern] = None
+            except Exception as e:
+                print('failed on filename: ', filename)
+
+    return render_template('index.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
