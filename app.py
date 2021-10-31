682fd50c2a0df4f902668543350900ff33401e71
BigBiff-hub <77340730+BigBiff-hub@users.noreply.github.com>
2021-10-31 02:22:40 +0000

    symbol -> ticker

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os, csv
 import talib
 import yfinance as yf
 import pandas
-from flask import Flask,  request, render_template
+from flask import Flask, request, render_template
 from patterns import candlestick_patterns
 
 app = Flask(__name__)
+
 
 @app.route('/capture')
 def capture():
@@ -21,9 +22,10 @@ def capture():
         "code": "success"
     }
 
+
 @app.route('/')
 def index():
-    pattern  = request.args.get('pattern', False)
+    pattern = request.args.get('pattern', False)
     stocks = {}
 
     with open('data/s&p500.csv') as f:
@@ -34,19 +36,19 @@ def index():
         for filename in os.listdir('data/daily'):
             df = pandas.read_csv(f'data/daily/{filename}')
             pattern_function = getattr(talib, pattern)
-            symbol = filename.split('.')[0]
+            ticker = filename.split('.')[0]
 
             try:
                 results = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
                 last = results.tail(1).values[0]
 
                 if last > 0:
-                    stocks[symbol][pattern] = 'bullish'
+                    stocks[ticker][pattern] = 'bullish'
                 elif last < 0:
-                    stocks[symbol][pattern] = 'bearish'
+                    stocks[ticker][pattern] = 'bearish'
                 else:
-                    stocks[symbol][pattern] = None
+                    stocks[ticker][pattern] = None
             except Exception as e:
-                print('Pattern failed on : ', filename)
+                print(f'Pattern failed on :{filename}')
 
     return render_template('index.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
